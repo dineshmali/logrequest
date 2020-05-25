4ec316efccf3b418d3fda716ff1ba3181dbcde63
Dinesh Mali <dmali085@gmail.com>
2020-05-25 15:23:49 +0530

    working sss

--- src/main.go
+++ src/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	input := os.Args
 	CaseNumber := input[1]
+	str := "sss"
+	check := contains(input, str)
+	if check {
+		words := []string{CaseNumber, "SHOW", "SUB", "SUM"}
+		joined := strings.Join(words, "_")
+		wordsnew := []string{"SR", joined}
+		joined = strings.Join(wordsnew, "")
+		fmt.Printf("ssh user@[Controller0IP] SHOW SUB SUB > %s_C0.txt\n", joined)
+		fmt.Printf("ssh user@[Controller1IP] SHOW SUB SUB > %s_C1.txt\n", joined)
+	}
 
-	fmt.Println(CaseNumber)
-
-	check := contains(input, "sss")
-	fmt.Println(check)
 }
 
 func contains(array []string, str string) bool {
