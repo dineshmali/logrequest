2d48cc6c6512c6daacb59d37655812daad5a4714
Dinesh Mali <dmali085@gmail.com>
2020-05-25 14:21:20 +0530

    contains success

--- src/main.go
+++ src/main.go
@@ -7,20 +7,17 @@ import (
 
 func main() {
 	input := os.Args
-
 	CaseNumber := input[1]
 
 	fmt.Println(CaseNumber)
 
-	switch checkword {
-	case checkword(input, sss):
-		println("sss asked")
-	}
+	check := contains(input, "sss")
+	fmt.Println(check)
 }
 
-func checkword(input []string, word string) bool {
-	for i, n := range input {
-		if n == word {
+func contains(array []string, str string) bool {
+	for _, a := range array {
+		if a == str {
 			return true
 		}
 	}
